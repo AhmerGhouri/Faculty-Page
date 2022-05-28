890712cee2a4a12ff3c751a869f9880e9a63eb93
Muhammad Ahmer Ghouri <ahmer7740@jmc.edu>
2022-05-28 19:06:23 +0500

    Working On Read More Button Using Api

--- Faculty.js
+++ Faculty.js
@@ -344,6 +344,8 @@ function getcards(DeptId) {
                 const element = cards[i];
 
 
+                // console.log(element)
+
                 // if (element.deptId === cardId) {
 
 
@@ -392,7 +394,7 @@ function getcards(DeptId) {
                             <div class="butn">
     
     
-                                    <button type="button" class="btn btn-primary"
+                                    <button onclick=popupData(${element.EmpId}) data-toggle="modal" data-target="#exampleModalLong" type="button" class="btn1 btn-primary"
                                     style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: 1.5rem; --bs-btn-font-size: .90rem;">
                                     Read More
                                     </button>
@@ -400,6 +402,7 @@ function getcards(DeptId) {
                                     
                                     </div>
     
+                                   
     
                                     </div>
     
@@ -412,7 +415,7 @@ function getcards(DeptId) {
     
                                     </div>`
 
-
+        
 
 
 
@@ -426,6 +429,160 @@ function getcards(DeptId) {
 
 
 
+
+}
+
+
+async function popupData(Id){
+
+
+    await fetch(`https://local.sohailuniversity.edu.pk:90/Handlers/SuWebFacultyHandlerDetail.ashx?empid=${Id}`)
+
+    .then(function(response) {
+
+        return response.json()
+
+    })
+
+    .then(function (data) {
+
+        var detData = data.NewEventsResultsData
+
+        var header = document.getElementById("header")
+
+        var cardDetail = document.getElementById("cardDetail")
+
+        
+        for (let i = 0; i < detData.length; i++) {
+            const detail = detData[i];
+            
+
+            header.innerHTML=`
+            
+            
+                    <div class="imgDiv">
+                
+                
+                        <div class="image">
+            
+            
+                            <img class="mainimg" src="/img/Dr-Meesam-Iftekhar-Hussain-Rizvi-p7ho61yjmsmnj8epq1nnga04ixmhxr9xy2am4rzmxc.png" alt="">
+            
+            
+            
+                        </div>
+
+
+                        <div class="detContDiv">
+
+                            <div class="nameHeading">
+                
+                
+                                <span>${detail.EmpName}</span>
+        
+        
+                            </div>
+                        
+                        
+                            <div class="designHeading">
+        
+        
+                                <span>${detail.DesignationName}</span>
+        
+        
+                            </div>
+                            
+                            <div class="designHeading">
+        
+        
+                                <span>${detail.DepartmentName}</span>
+        
+        
+                            </div>
+            
+                        </div>
+            
+                    </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+
+                                    <span aria-hidden="true">&times;</span>
+
+                                </button>
+
+                                
+
+
+            `
+            console.log(data)
+
+            var ExpData = data.Lst_Exp
+
+            for (let j = 0; j < ExpData.length; j++) {
+                const exp = ExpData[j];
+
+                cardDetail.innerHTML = `
+                
+                <div class="card-header" id="headingOne">
+
+                                                    <h5 class="mb-0">
+
+                                                        <button class="btn btn-link" data-toggle="collapse"
+                                                            data-target="#collapseOne" aria-expanded="true"
+                                                            aria-controls="collapseOne">
+
+                                                            Collapsible Group Item #1
+
+                                                        </button>
+
+                                                    </h5>
+
+                                                </div>
+
+
+                                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                                    data-parent="#accordion">
+
+                                                    <div class="card-body">
+
+
+                                                        <div class="eduList">
+
+                                                            <li><i class="fa fa-regular fa-circle-dot"></i>M.Phil.
+                                                                (Anatomy) = Baqai Medical University, Karachi (2016)
+                                                            </li>
+                                                            <li><i class="fa fa-regular fa-circle-dot"></i>M.B. B.S. =
+                                                                Hamdard University, Karachi (2003)
+                                                            </li>
+
+                                                        </div>
+
+                                                        Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                        accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                                                        non cupidatat skateboard dolor brunch. Food truck quinoa
+                                                        nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                                                        put a bird on it squid single-origin coffee nulla assumenda
+                                                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                                                        wes anderson cred nesciunt sapiente ea proident. Ad vegan
+                                                        excepteur butcher vice lomo. Leggings occaecat craft beer
+                                                        farm-to-table, raw denim aesthetic synth nesciunt you probably
+                                                        haven't heard of them accusamus labore sustainable VHS.
+
+                                                    </div>
+
+                                                </div>
+
+                `
+                
+            }
+
+
+            
+        }
+
+
+        
+    })
+    
 
 }
 
@@ -463,4 +620,3 @@ function active(DeptId) {
 
 
 }
-
